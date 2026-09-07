1fda7953292059eed842496baf26249a7265a36f
izabely_da_luz <izabelydaluzfernandes@gmail.com>
2026-09-07 10:52:38 -0300

    help

--- Firebase/firebaseConfig.js
+++ Firebase/firebaseConfig.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
-
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
- 
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -12,9 +10,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
- 
+
+console.log("Firebase config:", firebaseConfig); 
+
 const app = initializeApp(firebaseConfig);
- 
+
 export const auth = getAuth(app);
- 
 export const db = getFirestore(app);
